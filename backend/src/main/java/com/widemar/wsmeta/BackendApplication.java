ccbf53ea86f4688155d029b96ed6b16fffbb1643
widemar <widemarinternship@gmail.com>
2022-07-17 08:27:01 -0300

    Banco de Dados

---
+++ backend/src/main/java/com/widemar/wsmeta/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.widemar.wsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
